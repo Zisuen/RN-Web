1e89817ed26ea646381844e57f44148c961c70c0
Zisuen <urbanovsky97@gmail.com>
2023-05-04 16:13:58 +0200

    Creating a new version, that accomondas both targets

--- App.tsx
+++ App.tsx
@@ -1,29 +1,19 @@
-import "react-native-gesture-handler";
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import ThemesContextProvider from "./store/ThemesContext";
-import { store } from "./store/store";
-import { Provider } from "react-redux";
-
-import HomeScreen from "./screens/HomeScreen";
-import AddRecipeScreen from "./screens/AddRecipeScreen";
-
-const Stack = createNativeStackNavigator();
-
+import { View, Text, StyleSheet } from "react-native";
 const App = () => {
   return (
-    <Provider store={store}>
-      <ThemesContextProvider>
-        <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="AddRecipe" component={AddRecipeScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </ThemesContextProvider>
-    </Provider>
+    <View style={styles.container}>
+      <Text>App</Text>
+    </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- README.txt
+++ README.txt
@@ -1,1 +1,4 @@
 - yarn expo start
+
+- Web 950 UP
+- Mobile view anything below 950
