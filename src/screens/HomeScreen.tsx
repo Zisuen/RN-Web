6ae021d70f5bdcff1f146547e637abe78e0345fd
Zisuen <urbanovsky97@gmail.com>
2023-05-05 17:05:07 +0200

    Before changes

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,50 +1,14 @@
 import React, { useContext, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Modal,
-  Dimensions,
-} from "react-native";
-
 import RootContainer from "../components/RootContainer/RootContainer";
 import InnerContainer from "../components/InnerContainer";
 import { StyleContext } from "../store/StyleContext";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 import DataContainer from "../components/HomeScreen/DataContainer";
 import ShowRecipesBtn from "../components/HomeScreen/ShowRecipesBtn";
 import Recipes from "../components/HomeScreen/Recipes";
+import PrintRecipe from "../components/HomeScreen/PrintRecipe";
 
 const HomeScreen = () => {
-  const { resize, isPhone, theme } = useContext(StyleContext);
-  const top = useSafeAreaInsets().top;
-  const styles = StyleSheet.create({
-    recipesContainer: {
-      flex: 1,
-      marginTop: isPhone && top + 70,
-      paddingBottom: 10,
-      paddingTop: isPhone && 20,
-      paddingHorizontal: isPhone && 10,
-      alignItems: "center",
-      backgroundColor: "#70f892",
-      width: isPhone && "95%",
-      height: isPhone && "100%",
-      alignSelf: isPhone ? "center" : "baseline",
-      borderRadius: isPhone && 25,
-    },
-    displayContainer: {
-      flex: 3,
-      width: isPhone && "100%",
-      backgroundColor: "#91f8fc",
-    },
-    testItem: {
-      width: 150,
-      height: 40,
-      marginBottom: 10,
-      backgroundColor: "pink",
-    },
-  });
+  const { isPhone } = useContext(StyleContext);
   const [modal, setModal] = useState(false);
   const modalHandler = () => {
     setModal((current) => !current);
@@ -56,9 +20,7 @@ const HomeScreen = () => {
         <DataContainer>
           {isPhone && <ShowRecipesBtn showModal={modalHandler} />}
           <Recipes status={modal} closeModal={modalHandler} />
-          <View style={styles.displayContainer}>
-            <Text>Recipe Print Out</Text>
-          </View>
+          <PrintRecipe />
         </DataContainer>
       </InnerContainer>
     </RootContainer>
